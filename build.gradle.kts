5c751e3944e8a22a3f10bdf82b35e625289cc7a5
Artem Tsaryuk <42806772+funcid@users.noreply.github.com>
2024-04-28 01:03:16 +0300

    optimize build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,33 +9,11 @@ group = "me.func"
 version = "1.0-SNAPSHOT"
 
 repositories {
-	mavenLocal()
 	mavenCentral()
 }
 
 dependencies {
-	implementation(files("./libs/ASN1P.jar"))
-	implementation(files("./libs/AdES-core.jar"))
-	implementation(files("./libs/CAdES.jar"))
-	implementation(files("./libs/J6CF.jar"))
-	implementation(files("./libs/J6Oscar.jar"))
-	implementation(files("./libs/JCP.jar"))
-	implementation(files("./libs/JCSP.jar"))
-	implementation(files("./libs/JCPControlPane.jar"))
-	implementation(files("./libs/JCPRequest.jar"))
-	implementation(files("./libs/JCPRevCheck.jar"))
-	implementation(files("./libs/JCPRevTools.jar"))
-	implementation(files("./libs/JCPxml.jar"))
-	implementation(files("./libs/JCryptoP.jar"))
-	implementation(files("./libs/Rutoken.jar"))
-	implementation(files("./libs/XAdES.jar"))
-	implementation(files("./libs/XMLDSigRI.jar"))
-	implementation(files("./libs/asn1rt.jar"))
-	implementation(files("./libs/cmsutil.jar"))
-	implementation(files("./libs/sspiSSL.jar"))
-	implementation(files("./libs/cpSSL.jar"))
-	implementation(files("./libs/tls_proxy.jar"))
-	implementation(files("./libs/forms_rt.jar"))
+	implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 	testImplementation(kotlin("test"))
 }
 
